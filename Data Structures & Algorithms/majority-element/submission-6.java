10c0d4ada9d9e8c63164202a9365de599fa8d81a
Rakesh Goud Edigi <grakee231@gmail.com>
2026-09-13 09:40:45 -0400

    Add: majority-element - submission-6

---
+++ Data Structures & Algorithms/majority-element/submission-6.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int majorityElement = nums[0];
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (count == 0) {
+                majorityElement = nums[i];
+            }
+            if (nums[i] == majorityElement) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return majorityElement;
+    }
+}
