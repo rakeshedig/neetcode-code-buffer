f0483cacf2ef271cae97412fb53a7d99dd6aa7e8
Rakesh Goud Edigi <grakee231@gmail.com>
2026-09-13 18:46:32 -0400

    Add: sort-an-array - submission-5

---
+++ Data Structures & Algorithms/sort-an-array/submission-5.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+
+        for(int i =0; i<nums.length;i++){
+
+                int temp=0;
+            for(int j=i+1; j<nums.length;j++)
+            {
+                if(nums[i]>nums[j]){
+
+                    temp = nums[i];
+                    nums[i]=nums[j];
+                    nums[j]=temp;
+                }
+            }
+           
+        }
+
+         return nums;
+    }
+}
